29ea10f42f481d7f9a75da2fad8afc30989cf847
EmmaMatevosyan <131385066+EmmaMatevosyan@users.noreply.github.com>
2023-11-18 22:57:57 +0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,21 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    public static void main(String[] args) throws Exception {
+        MathCalc calc = new MathCalc(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String input = scanner.nextLine();
+
+        try {
+            System.out.println(calc.calc(input));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
     }
 }
